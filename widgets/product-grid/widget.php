478a531a16b8099de71f071cb9e2563e3b8cde8a
wisdomrainmusic <alisverissepetiniz4601@gmail.com>
2025-11-27 11:15:42 +0300

    Add minimal WR Product Grid widget skeleton

--- widgets/product-grid/widget.php
+++ widgets/product-grid/widget.php
@@ -1,2 +1,30 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+use Elementor\Widget_Base;
+
+class WR_EW_Product_Grid extends Widget_Base {
+
+    public function get_name() {
+        return 'wr-product-grid';
+    }
+
+    public function get_title() {
+        return 'WR Product Grid';
+    }
+
+    public function get_icon() {
+        return 'eicon-products';
+    }
+
+    public function get_categories() {
+        return [ 'wr-widgets' ];
+    }
+
+    protected function render() {
+        echo '<div class="wr-product-grid-test">';
+        echo '<div class="wr-product-grid-sidebar">Sidebar Test</div>';
+        echo '<div class="wr-product-grid-items">Grid Test</div>';
+        echo '</div>';
+    }
+}
